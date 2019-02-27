142fc0016d7a2bde534657c5e06d5d12b8201ee2
Edward Ding <eding2019@gmail.com>
2019-02-27 22:47:26 +0000

    Add input function python script

---
+++ inputfunction.py
@@ -0,0 +1,7 @@
+#We'll be exploring the input function, or input()
+
+firstinput = input("Here! Look! I'm asking you for a input!")
+#This sets the string firstinput as whatever you decide to enter. 
+
+print(firstinput)
+#This prints whatever you entered in the first line
