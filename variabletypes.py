3b3d581003ba45433d62bf3fb728cdbab2a0b39b
Edward Ding <eding2019@gmail.com>
2019-02-27 22:12:27 +0000

    Add variable types

---
+++ variabletypes.py
@@ -0,0 +1,17 @@
+#Variable Types
+
+
+#Integers are simply numbers.
+int = 21
+
+#Strings are simply strings of words, like sentences.
+
+lol = "Thanos did nothing wrong."
+
+#Floats are numbers with decimal points.
+
+float = 3.1415
+
+
+
+
