b9808f8299ef095c864156dd0a8e1028707728af
Edward Ding <eding2019@gmail.com>
2019-02-27 22:05:11 +0000

    add printfunction lesson plan

---
+++ PrintFunction.py
@@ -0,0 +1,16 @@
+print("Hello World")
+#Prints Hello World i guess
+
+print("I hate you")
+
+#Prints "I hate you"
+
+#Click "run" to see the results!
+
+print("\nThere is a new line before this one now!")
+
+#The special phrase \n adds a new line/line break.
+
+#You can also use a blank print statement to accomplish the same purpose. 
+
+print("")
