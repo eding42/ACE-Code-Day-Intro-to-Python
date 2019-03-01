7a237a3189a4cbef1ca14401f927f932981f0639
Edward Ding <eding2019>
2019-02-28 21:49:41 -0800

    add subtraction to simple calculator, unfinished

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-firstnum = 2
+firstnum = 3
 #Sets first number as 2
 
-secondnum = 3
+secondnum = 2
 #Sets second number as 3
 
 result = firstnum+secondnum
 
+print(result)
+
 #wow 2+3 equals 5
 
+result = firstnum-secondnum
+
+print(result)
