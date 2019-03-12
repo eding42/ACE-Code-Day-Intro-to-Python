2c564df93284071d2f075fb0d83600e33fed97fd
Edward Ding <eding2019>
2019-03-11 22:45:18 -0700

    addition function, kinda bad i know i need sleep fix it vincent

--- complexcalculator.py
+++ complexcalculator.py
@@ -1,1 +1,14 @@
-#text here
+x,y=input()
+
+
+operation=str(input("Hello there, what is your operation?"))
+
+x = int(x)
+y = int(y)
+
+def addition():
+    result=x+y
+    print(result)
+
+if operation.lower() == "addition":
+    addition()
