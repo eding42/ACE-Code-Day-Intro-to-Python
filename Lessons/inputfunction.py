53399dc654cfa8ec1a0524e5ae84961dc5d34bc5
Edward Ding <eding2019>
2019-03-12 21:43:40 -0700

    GOD REORGANIZED ALL OF THE FILES INTO RELEVANT FOLDERS THAT TOOK A LOT OF CLICKING THROUGH DIALOG BOXES THANKS PYCHARM

--- Lessons/inputfunction.py
+++ Lessons/inputfunction.py
@@ -1,7 +1,6 @@
 # We'll be exploring the input function, or input()
-
-
-# This sets the string firstinput as whatever you decide to enter. 
+# The input function allows for command line level interaction with the user.
+# This sets the string firstinput as whatever you decide to enter.
 firstinput = input("Here! Look! I'm asking you for a input: ")
 
 # This prints whatever you entered in the first line
