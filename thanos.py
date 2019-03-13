7c19b9952144883d3e5e8c69649ddf1b813ff7ba
Edward Ding <eding2019>
2019-03-12 21:12:46 -0700

    Fixed thanos.py, and added additional printouts.
    oh and also added fortnite and T-series

--- thanos.py
+++ thanos.py
@@ -1,18 +1,22 @@
 import random
 # A Thanos Snap simulator
-# This thanos script takes a list of people and snaps them randomly
+# This thanos script takes a list of people and snaps them randomly - hopefully
 
-# Create list of people
-people = ["vincent", "edward", "Fortnite", "Chungus", "t-series"]
+# Creates a normanl list of normal people.
+
+people = ["Vincent", "Edward", "Fortnite", "Chungus", "T-Series"]
 
 # Delete half the people
 # Deletes one person len(people)//2 times
 for i in range(len(people)//2):
     #Select a random person
-    snapped = random.choice(people)
+    snapped = str(random.choice(people))
 
     # Snap the person
+    print("\n" + snapped + " has been snapped.")
     people.remove(snapped)
 
 # Print remaining
-print(people)
+print("\nThe people reminaing are: " + str(people))
+
+print("\nSmile... for even in death, you will have become children of Thanos!")
