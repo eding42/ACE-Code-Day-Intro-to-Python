b707f1f65266c4ea0aab159e7a02cfc53d800a56
bbworld <vw1212@pleasantonusd.net>
2019-03-12 20:08:32 -0700

    Fix up calculator (it's stila basic one but who cares), Add actually decent explanation to kill computer script, and lecture Edward about how to actually use whitespace in Python.

--- inputfunction.py
+++ inputfunction.py
@@ -1,7 +1,8 @@
-#We'll be exploring the input function, or input()
+# We'll be exploring the input function, or input()
 
+
+# This sets the string firstinput as whatever you decide to enter. 
 firstinput = input("Here! Look! I'm asking you for a input!")
-#This sets the string firstinput as whatever you decide to enter. 
 
+# This prints whatever you entered in the first line
 print(firstinput)
-#This prints whatever you entered in the first line
--- simplecalculator.py
+++ simplecalculator.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# NOTE TO EDWARD: this is NOT HOW YOU USE WHITESPACE
+# If you put a newline after EVERY line you might as well
+# not put the newline there in the first place
 
 firstnum = 3
 #Sets first number as 2
